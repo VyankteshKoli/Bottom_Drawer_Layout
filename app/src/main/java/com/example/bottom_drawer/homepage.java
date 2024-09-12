dc069d34914bd92e99c196295dac795f985cb897
VyankteshKoli <141150425+VyankteshKoli@users.noreply.github.com>
2024-09-12 12:23:19 +0530

    main
    desc

---
+++ app/src/main/java/com/example/bottom_drawer/homepage.java
@@ -0,0 +1,127 @@
+package com.example.bottom_drawer;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.bottom_drawer.AboutFragment;
+import com.example.bottom_drawer.HomeFragment;
+import com.example.bottom_drawer.SettingsFragment;
+import com.example.bottom_drawer.ShareFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+/** @noinspection ALL*/
+public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+
+    BottomNavigationView bottomnavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //bottomnav
+        bottomnavigationView = findViewById(R.id.bottomnavigationView);
+
+        bottomnavigationView.setOnNavigationItemSelectedListener(nav_listner);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener nav_listner= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            int a= item.getItemId();
+            if (a == R.id.bottom_home) {
+                selectedFragment = new HomeFragment();
+            }
+            else if (a==R.id.bottom_chatbot)
+            {
+                selectedFragment= new ChatbotFragment();
+            }
+            else if (a==R.id.bottom_profile)
+            {
+                selectedFragment= new ProfileFragment();
+            }
+            else
+            {
+                selectedFragment = new HomeFragment();
+            }
+
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            FragmentTransaction transaction=fragmentManager.beginTransaction();
+            transaction.replace(R.id.fragment_container,selectedFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+            return true;
+        }
+    };
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.nav_home)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        }
+        else if(id == R.id.nav_settings)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+        }
+        else if(id == R.id.nav_share)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShareFragment()).commit();
+        }
+        else if(id == R.id.nav_about)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
+        }
+//            else if(id == R.id.nav_logout)
+//            {
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
+//            }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
